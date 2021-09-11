03e5172486db23f1242f1e371d7335ba784eb105
Aquafir <aquafirs@gmail.com>
2021-09-11 15:50:44 -0500

    /raiserefundto only targets online temporarily

--- Source/ACE.Server/DuskfallMods/DuskfallAdminCommands.cs
+++ Source/ACE.Server/DuskfallMods/DuskfallAdminCommands.cs
@@ -19,21 +19,24 @@ namespace ACE.Server.Command.Handlers
             DuskfallRaise.RaiseRefundToPlayer(session.Player);
         }
 
-        [CommandHandler("raiserefundto", AccessLevel.Admin, CommandHandlerFlag.None, 1, "Refunds costs associated with /raise.", "/raiserefund [*|name]")]
+        [CommandHandler("raiserefundto", AccessLevel.Admin, CommandHandlerFlag.None, 1, "Refunds costs associated with /raise.", "/raiserefund [*|name|id]")]
         public static void HandleRaiseRefundTo(Session session, params string[] parameters)
         {
+            //Todo: Handle offline players by adding properties directly to the helper?
             //Refund all players
             if (parameters[0] == "*")
             {
-                PlayerManager.GetAllPlayers().ForEach(p => DuskfallRaise.RaiseRefundToPlayer((Player)p));
+                //PlayerManager.GetAllPlayers().ForEach(p => DuskfallRaise.RaiseRefundToPlayer((Player)p));
+                PlayerManager.GetAllOnline().ForEach(p => DuskfallRaise.RaiseRefundToPlayer(p));
                 return;
             }
 
             //Refund by name/ID
-            var player = PlayerManager.FindByName(parameters[0]) as Player;
+            //var player = PlayerManager.FindByName(parameters[0]) as Player;
+            var player = PlayerManager.GetOnlinePlayer(parameters[0]);
             if (player == null)
             {
-                ChatPacket.SendServerMessage(session, $"No player named {parameters[0]} found.", ChatMessageType.Broadcast);
+                ChatPacket.SendServerMessage(session, $"No player {parameters[0]} found.", ChatMessageType.Broadcast);
                 return;
             }
 
